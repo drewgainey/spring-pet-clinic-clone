f68d0f37762b0832b61786a4cbf3ce5003606bd3
Marcus Gainey <mgain89@wgu.edu>
2023-10-11 14:29:20 -0400

    initialized spring project

---
+++ src/main/java/com/example/springpetclinicclone/SpringPetClinicCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpetclinicclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetClinicCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springpetclinicclone/SpringPetClinicCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpetclinicclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
